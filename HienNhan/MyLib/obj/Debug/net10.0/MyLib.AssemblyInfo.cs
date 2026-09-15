4e98bc388d451a18ca68486b0a7a9ab2e3ed7792
Ngdang1512 <3123411072@sv.sgu.edu.vn>
2026-09-15 10:05:40 +0700

    pull MinhVy

--- HienNhan/MyLib/obj/Debug/net10.0/MyLib.AssemblyInfo.cs
+++ HienNhan/MyLib/obj/Debug/net10.0/MyLib.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b9827ae22ce0d6020e95481ff791bbdd0d5019a")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+475d8d5ca10e3b8845004309ba105806a766d349")]
->>>>>>> 86bc5fde06d783ab01c6df86105e0ca40b7ab353
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce5c44f1f09b48e51642b1ddfa70cf0b79c518c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- NgDang/Buoi04.Bai02/obj/Debug/net10.0/Buoi04.Bai02.AssemblyInfo.cs
+++ NgDang/Buoi04.Bai02/obj/Debug/net10.0/Buoi04.Bai02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Buoi04.Bai02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d16ceab09b1b21debcf0ec2bd6407c8c79b152d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce5c44f1f09b48e51642b1ddfa70cf0b79c518c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Buoi04.Bai02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Buoi04.Bai02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
