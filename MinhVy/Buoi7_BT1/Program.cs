c6775ffb4768bf8662e6828837b5ae76a436352c
MinhVy1324 <vominhvy0410@gmail.com>
2026-09-17 22:32:16 +0700

    buoi7_BT1

--- MinhVy/Buoi7_BT1/Program.cs
+++ MinhVy/Buoi7_BT1/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using MyLib;
+namespace Buoi7_BT1;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("hay nhap chieu cao h");
+        int h=int.Parse(Console.ReadLine());
+        int [] list=new int[]{h,h+2,h+4};
+        foreach(int c in list){
+            Console.WriteLine($"h={c}");
+            List<string> tamGiac=Buoi7_InHinhTamGiacVaVuongLib.InHinh(c);
+            foreach(string dong in tamGiac)
+            {
+                Console.WriteLine(dong);
+            }
+            
+        }
+    }
+}
